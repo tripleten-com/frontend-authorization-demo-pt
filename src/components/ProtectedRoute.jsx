b082e2535862bf36c20d61cff15578836e690b24
Vladislav Pavlov <vldslw@gmail.com>
2024-10-22 13:48:29 -0300

    add content

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,24 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
-function ProtectedRoute({ isLoggedIn, children }) {
-  if (!isLoggedIn) {
-    return <Navigate to="/login" replace />;
+export default function ProtectedRoute({
+  isLoggedIn,
+  children,
+  anonymous = false,
+}) {
+  const location = useLocation();
+  const from = location.state?.from || "/";
+  // Se o usuário estiver logado, vamos redirecioná-lo
+  // para longe das rotas anônimas.
+  if (anonymous && isLoggedIn) {
+    return <Navigate to={from} />;
   }
+
+  // Se o usuário não estiver logado e tentar acessar uma rota que
+  // requer autorização, vamos redirecioná-lo para a rota /login.
+  if (!anonymous && !isLoggedIn) {
+    return <Navigate to="/login" state={{ from: location }} />;
+  }
+
+  // Caso contrário, exiba os elementos filhos da rota atual.
   return children;
 }
-
-export default ProtectedRoute;
