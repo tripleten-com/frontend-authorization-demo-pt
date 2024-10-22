a813a5598d1fda9a433329c5fe14a94b3821c805
Vladislav Pavlov <vldslw@gmail.com>
2024-10-22 13:22:23 -0300

    add content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
-import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import Ducks from "./Ducks";
 import Login from "./Login";
 import MyProfile from "./MyProfile";
@@ -6,6 +8,7 @@ import Register from "./Register";
 import "./styles/App.css";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
     <Routes>
       <Route path="/ducks" element={<Ducks />} />
@@ -24,6 +27,16 @@ function App() {
           <div className="registerContainer">
             <Register />
           </div>
+        }
+      />
+      <Route
+        path="*"
+        element={
+          isLoggedIn ? (
+            <Navigate to="/ducks" replace />
+          ) : (
+            <Navigate to="/login" replace />
+          )
         }
       />
     </Routes>
