2e8ff5450671448f23dfdfe60eda831916c257ff
Vladislav Pavlov <vldslw@gmail.com>
2024-11-07 11:29:47 -0300

    fix placeholders text

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -2,7 +2,7 @@ import NavBar from "./NavBar";
 import "./styles/MyProfile.css";
 
 function MyProfile({
-  userData = { username: "Username here", email: "Email here" },
+  userData = { username: "Nome de usuário aqui", email: "E-mail aqui" },
   setIsLoggedIn,
 }) {
   const { username, email } = userData;
